724297fb0d6697610ec9546502052f2d5c690504
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-23 09:18:47 +0530

    msg add

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,16 @@
+#!/bin/bash -x
+
 echo "Welcome to Employee wage program"
+
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
+else
+        salary=0;
+fi
